c10636807c0ad46454e4e20864e7c48f943d74cf
AngieGon <31994962+AngieGon@users.noreply.github.com>
2017-09-18 10:31:16 -0500

    Update PilaConsola2.java

--- PilaConsola2.java
+++ PilaConsola2.java
@@ -30,87 +30,102 @@ public class NodoPila {
     }
     
     public static void imprimirPila(NodoPila Pila){
-           NodoPila temporal=Pila;
-        if(NodoPila.pilaVacia(temporal))
-            System.out.println("La pila no contiente elementos..");
-        else{
-            int numeroElemento=0;
-            while(temporal!=null)
-            {
-                System.out.printf("\nImprimiento elemento Nro. %d: %s \n",numeroElemento,temporal.dato);
+          NodoPila temporal = Pila;
+
+        if (NodoPila.pilaVacia(temporal)) {
+            System.out.println("La Cola no contiente elementos..");
+            JOptionPane.showMessageDialog(null,"La Cola no contiente elementos.." );
+        } else {
+            int numeroElemento = 0;
+            while (temporal != null) {
+                System.out.printf("\nImprimiento elemento Nro. %d: %s \n", numeroElemento, temporal.dato);
+                MenuPrincipal.TXTimprimir.setText(MenuPrincipal.TXTimprimir.getText() + temporal.dato + "-");
                 numeroElemento++;
-                temporal=temporal.enlace;
+                temporal = temporal.enlace;
             }
         }
     }
+
     
     public static void buscarPila(NodoPila Pila, String datoBusqueda){
-        NodoPila temporal=Pila;
-        if(NodoPila.pilaVacia(temporal))
+       NodoPila temporal = Pila;
+        if (NodoPila.pilaVacia(temporal)) {
             System.out.println("La pila no contiente elementos..");
-        else{
-            int numeroElemento=0;
-            while(temporal!=null)
-            {
-                if(datoBusqueda.equals(temporal.dato))
-                {
-                    System.out.printf("\nEl elemento %s esta en la posición: %d de la pila \n",datoBusqueda,numeroElemento);
+            JOptionPane.showMessageDialog(null,"La pila no contiente elementos.." );
+        } else {
+            int numeroElemento = 0;
+            while (temporal != null) {
+                if (datoBusqueda.equals(temporal.dato)) {
+                System.out.println("El elemento si se encuentra en la pila.");
+                JOptionPane.showMessageDialog(null,"El elemento si se encuentra en la pila." );
+                 
                     break;
                 }else{
-                    System.out.println("Elemento no encontrado");
-                    break;
-             
-                }
+                          System.out.println("El elemento" + datoBusqueda.toUpperCase() +" no se encuentra en la pila."); 
+                           JOptionPane.showMessageDialog(null,"El elemento " + datoBusqueda.toUpperCase() + " no se encuentra en la Pila." );
+                          break;
+                            }
                 }
                 numeroElemento++;
-                temporal=temporal.enlace;
-                
+                temporal = temporal.enlace;
+               
             }
-    }
+        }
     
     
     public static NodoPila insertarPila(NodoPila Pila, String dato){
-        NodoPila nodoNuevo=new NodoPila(dato);
-        if(NodoPila.pilaVacia(Pila))
-          Pila=nodoNuevo;
-        else
-        {
-            nodoNuevo.enlace=Pila;
-            Pila=nodoNuevo;
+   NodoPila nodoNuevo = new NodoPila(dato);
+        if (NodoPila.pilaVacia(Pila)) {
+            Pila = nodoNuevo;
+        } else {
+            NodoPila anterior = new NodoPila();
+            NodoPila temporal = Pila;
+            while (temporal != null) {
+                anterior = temporal;
+                temporal = temporal.enlace;
+                
+            }
+            System.out.println("Elemento ingresado");
+            anterior.enlace = nodoNuevo;
         }
-        System.out.println("\"Elemento insertado\"");
         return Pila;
     }
     
+
+    
     public static NodoPila eliminarPila(NodoPila Pila){
-        if (NodoPila.pilaVacia(Pila))
-        {
-            System.out.println("La pila no tiene elementos");
+         if (NodoPila.pilaVacia(Pila)) {
+            System.out.println("La pila no contiente elementos..");
+            JOptionPane.showMessageDialog(null,"La pila no contiente elementos.." );
             return Pila;
         }
-        System.out.println("Se elimino el elemento " + Pila.dato);
+        System.out.println("Se ha eliminado del tope de la pila");
+        JOptionPane.showMessageDialog(null,"Se ha eliminado del tope de la pila" );
         Pila = Pila.enlace;
         return Pila;
     }
-
     public static boolean pilaVacia(NodoPila Pila){
         if (Pila==null)
             return true;
         return false;
     }
     public static void imprimirprimerelementodelaPila(NodoPila Pila) {
-           NodoPila temporal=Pila;
-        if(NodoPila.pilaVacia(temporal))
+    NodoPila temporal = Pila;
+        NodoPila primero = temporal;
+
+        if (NodoPila.pilaVacia(temporal)) {
             System.out.println("La pila no contiente elementos..");
-        else{
-            int numeroElemento=0;
-            while(temporal!=null)
-            {
-                System.out.printf("\nImprimiento elemento Nro. %d: %s \n",numeroElemento,temporal.dato);
-                //numeroElemento++;
-                //temporal=temporal.enlace;
-            break;
+            JOptionPane.showMessageDialog(null,"La pila no contiente elementos.." );
+        } else {
+            int numeroElemento = 0;
+            while (temporal != null) {
+                if(temporal == primero){
+                System.out.printf("\nImprimiento el inicio elemento Nro. %d: %s \n", numeroElemento, temporal.dato);
+                MenuPrincipal.TXTimprimir.setText(temporal.dato);
+                }
+                numeroElemento++;
+                temporal = temporal.enlace;
             }
-        } 
+        }
     }
 }
